4abdf995180199cf6c081426322ec750cd7fd866
Nico88-Vs <112125692+Nico88-Vs@users.noreply.github.com>
2025-04-18 18:08:32 +0200

    old version

--- obj/Debug/DivergentStrV0-1.AssemblyInfo.cs
+++ obj/Debug/DivergentStrV0-1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DivergentStrV0_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acccdf2aba95e5ea9f596a48c3aace9ae5482392")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a6212ed1420ef0889b6011bd7420cf4d49ade22")]
 [assembly: System.Reflection.AssemblyProductAttribute("DivergentStrV0_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DivergentStrV0_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
